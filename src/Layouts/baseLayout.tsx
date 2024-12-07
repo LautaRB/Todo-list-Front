9e31f945c531478facf6ffbb021f3b780b4c9518
Lautaro Rodriguez Brown <lautanba@gmail.com>
2024-12-07 13:03:34 -0300

    Pequeña reestructuración del proyecto y creación del baseLayout

---
+++ src/Layouts/baseLayout.tsx
@@ -0,0 +1,9 @@
+function BaseLayout() {
+  return (
+    <div className="font-family: system-ui, sans-serif min-h-screen flex flex-col bg-black-222">
+      
+    </div>
+  );
+}
+
+export default BaseLayout;
