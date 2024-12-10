e48f567e2b2dba0d7663059620a5294c235af872
LautaRB <lautanb@gmail.com>
2024-12-10 16:25:15 -0300

    Pequeños cambios en el estilo y navbar.

--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -1,16 +1,16 @@
 function Navbar() {
 	return (
-		<nav className="fixed top-0 left-0 w-full flex justify-center backdrop-blur-sm space-x-6 py-4 z-50">
-			<a className="nav-link text-black hover:text-orange-700 transition-colors duration-300">
+		<nav className="font-sour-gummy fixed top-0 left-0 w-full flex justify-center space-x-6 py-4 z-50">
+			<a className="nav-link text-black text-xl hover:text-orange-700 transition-colors duration-300">
 				Elemento 1
 			</a>
-			<a className="nav-link text-black hover:text-orange-700 transition-colors duration-300">
+			<a className="nav-link text-black text-xl hover:text-orange-700 transition-colors duration-300">
 				Elemento 2
 			</a>
-			<a className="nav-link text-black hover:text-orange-700 transition-colors duration-300">
+			<a className="nav-link text-black text-xl hover:text-orange-700 transition-colors duration-300">
 				Elemento 3
 			</a>
-			<a className="nav-link text-black hover:text-orange-700 transition-colors duration-300">
+			<a className="nav-link text-black text-xl hover:text-orange-700 transition-colors duration-300">
 				Elemento 4
 			</a>
 		</nav>
--- src/Layouts/baseLayout.tsx
+++ src/Layouts/baseLayout.tsx
@@ -6,6 +6,7 @@ function BaseLayout() {
 		<div className="font-family: system-ui min-h-screen flex flex-col bg-yellow-300">
 			<header className="w-full py-4">
 				<Navbar />
+				<hr className="border-t-4 border-amber-800 my-10 mx-auto w-full" />
 			</header>
 			<main className="flex-grow container mx-auto py-8">
 				{/*contenido de la página */}
