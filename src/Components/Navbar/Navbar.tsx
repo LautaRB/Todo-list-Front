a9a6e2d35fb5f8a486a560f868d6da0220327f3a
LautaRB <lautanb@gmail.com>
2024-12-11 15:44:51 -0300

    cambio en el Navbar, imagen en proceso

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -2,11 +2,23 @@ import NavLink from './NavLink';
 
 function Navbar() {
 	return (
-		<nav className="font-sour-gummy fixed top-0 left-0 w-full flex justify-center space-x-6 py-4 z-50">
-			<NavLink>Elemento 1</NavLink>
-			<NavLink>Elemento 2</NavLink>
-			<NavLink>Elemento 3</NavLink>
-			<NavLink>Elemento 4</NavLink>
+		<nav className="font-sour-gummy fixed top-0 left-0 w-full flex justify-between space-x-6 py-4 z-50">
+			<div className="pl-6">
+				<NavLink>Todografo</NavLink>
+			</div>
+			<div>
+				<img
+					src="/Stickman.svg"
+					alt="stickman"
+					className="h-100 w-8" // Ajusta el tamaño aquí según tus necesidades
+				/>
+			</div>
+			<div className="space-x-4 pr-6">
+				<NavLink>Elemento 1</NavLink>
+				<NavLink>Elemento 2</NavLink>
+				<NavLink>Elemento 3</NavLink>
+				<NavLink>Elemento 4</NavLink>
+			</div>
 		</nav>
 	);
 }
