55c250c45c4ac9f187c8cfa6642bb32c01c2c6a5
Smilari <smilari.lucas@gmail.com>
2024-12-16 09:24:42 -0300

    Fix navbar oculta headers (#)

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { Stickman as StickmanIcon } from '@icons/Stickman.tsx';
 
 export const Navbar = () => {
 	return (
-		<nav className="grid grid-cols-2 h-16 w-full lg:w-5/6 m-auto">
+		<nav className="grid grid-cols-2 w-full lg:w-5/6 m-auto">
 			<a href="/" className="group flex items-center w-fit pl-1 lg:pl-9">
 				<StickmanIcon className="h-11 w-auto text-black" />
 				<span className="text-2xl font-heading -ml-1 group-hover:text-blue-600 transition-colors duration-300">
