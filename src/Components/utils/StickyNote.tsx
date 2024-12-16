8fc9684a31bd976c92bb5a058920dcb04d1fcfc9
Smilari <smilari.lucas@gmail.com>
2024-12-16 14:47:21 -0300

    Update de estilos para página de error

--- src/Components/utils/StickyNote.tsx
+++ src/Components/utils/StickyNote.tsx
@@ -1,5 +1,5 @@
 export const StickyNote: React.FC<{
-	title: string;
+	title?: string;
 	children: React.ReactNode;
 	color?: string;
 	className?: string;
@@ -7,7 +7,7 @@ export const StickyNote: React.FC<{
 	<div
 		className={`sticky-note bg-${color}-100 after:border-b-${color}-100 after:border-l-${color}-100 ${className}`}
 	>
-		<h3 className="text-lg font-bold mb-2">{title}</h3>
+		{title && <h3 className="text-lg font-bold mb-2">{title}</h3>}
 		<div className="font-handwritten">{children}</div>
 	</div>
 );
--- src/Pages/ErrorPage.tsx
+++ src/Pages/ErrorPage.tsx
@@ -1,13 +1,19 @@
+import { StickyNote } from '@components/utils/StickyNote';
+
 export const ErrorPage = () => {
 	return (
 		<div className="grid place-items-center h-screen w-11/12 m-auto lg:w-5/6">
-			<section className="section-container flex flex-col items-center justify-center h-42 space-y-8">
-				<h1 className="text-4xl font-bold font-heading text-red-500 text-center">
-					Página no encontrada [404]
-				</h1>
-				<a href="/" className="btn-red font-handwritten">
-					Volver al inicio
-				</a>
+			<section className="section-container flex flex-col items-center justify-center h-64">
+				<StickyNote color="yellow">
+					<div className="flex flex-col items-center justify-center gap-7">
+						<h1 className="text-5xl font-bold font-heading text-red-400 text-center">
+							404 <span className="text-3xl relative bottom-2">:(</span>
+						</h1>
+						<a href="/" className="btn-red font-handwritten">
+							Volver al inicio
+						</a>
+					</div>
+				</StickyNote>
 			</section>
 		</div>
 	);
