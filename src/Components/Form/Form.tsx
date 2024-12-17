175ef8f554c3ca9fce0aeb81aec00a32d0686567
Lautaro Rodriguez Brown <lautanba@gmail.com>
2024-12-16 22:01:22 -0300

    Form reutilizable

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,16 +1,19 @@
-export const Form = () => {
+interface Props {
+	title: string;
+    buttonText: string;
+    children: React.ReactNode;
+}
+
+export const Form : React.FC<Props> = ({ title, buttonText, children }) => {
     return (
         <form action="{action}" className="flex flex-col items-center justify-center gap-7">
             <h1 className="text-4xl font-bold font-heading text-black text-center">
-                Registro
+                {title}
             </h1>
             <div className="grid grid-cols-1 gap-2">
-                <input type="text" placeholder="Nombre de Usuario" required />
-                <input type="password" placeholder="Contraseña" required />
-                <input type="text" placeholder="Email" required />
-
-                <a href="#" className="btn-green font-handwritten">
-                    Registrarse
+                {children}
+                <a type="submit" className="btn-green font-handwritten">
+                    {buttonText}
                 </a>
             </div>
         </form>
--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -6,7 +6,11 @@ export const Register = () => {
 		<div className="grid place-items-center h-screen w-11/12 m-auto lg:w-5/6">
 			<section className="section-container flex flex-col items-center justify-center h-70">
 				<StickyNote color="yellow">
-					<Form />
+					<Form title="Registro" buttonText="Registrarse">
+						<input type="text" placeholder="Nombre de Usuario" required />
+						<input type="password" placeholder="Contraseña" required />
+						<input type="text" placeholder="Email" required />
+					</Form>
 				</StickyNote>
 			</section>
 		</div>
