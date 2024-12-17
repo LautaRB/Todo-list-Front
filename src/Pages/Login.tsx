b3418f12a7a2427fbe2afdfbff1b41b52db18b44
Lautaro Rodriguez Brown <lautanba@gmail.com>
2024-12-17 00:09:28 -0300

    Link de Registro a Login, y pequeño fix boton login

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -7,7 +7,7 @@ export const Login = () => {
 		<div className="grid place-items-center h-screen w-11/12 m-auto lg:w-5/6">
 			<section className="section-container flex flex-col items-center justify-center h-70">
 				<StickyNote color="yellow">
-					<Form title="Inicio de Sesión" buttonText="Iniciar Sesión">
+					<Form title="Inicio de Sesión" submitText="Iniciar Sesión">
 						<Input type="text" placeholder="Usuario o Email" required />
 						<Input type="password" placeholder="Contraseña" required />
 					</Form>
--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -6,12 +6,18 @@ export const Register = () => {
 	return (
 		<div className="grid place-items-center h-screen w-11/12 m-auto lg:w-5/6">
 			<section className="section-container flex flex-col items-center justify-center h-70">
-				<StickyNote color="yellow">
+				<StickyNote className="h-[222px]" color="yellow">
 					<Form title="Registro" submitText="Registrarse">
 						<Input type="text" placeholder="Usuario" required />
 						<Input type="password" placeholder="Contraseña" required />
 						<Input type="text" placeholder="Email" required />
 					</Form>
+					<a
+						href="http://localhost:5173/login"
+						className="absolute mt-4 text-center text-sm text-gray-500 hover:text-blue-600 transition-colors duration-300"
+					>
+						¿Ya tienes una cuenta?
+					</a>
 				</StickyNote>
 			</section>
 		</div>
