cc4dd25fd849a1b813b87877fd54ff0cc4f591dd
Smilari <smilari.lucas@gmail.com>
2024-12-22 16:38:48 -0300

    Animación al abrir el menú hamburguesa

--- src/Components/Header/Navbar.tsx
+++ src/Components/Header/Navbar.tsx
@@ -9,6 +9,7 @@ export const Navbar = () => {
 	const handleClick = () => {
 		setIsOpen(!isOpen);
 	};
+
 	const navLinks = [
 		{ name: 'Características', href: '#features' },
 		{ name: 'Cómo funciona', href: '#how-it-works' },
@@ -17,12 +18,14 @@ export const Navbar = () => {
 
 	return (
 		<nav className="grid grid-cols-2 w-mobile lg:w-desktop m-auto">
-			<a href="/" className="group flex items-center w-fit">
-				<StickmanIcon className="h-11 w-auto text-black" />
+			<a href="/" className="group flex items-center w-fit h-fit self-center">
+				<StickmanIcon className="h-10 w-auto text-black" />
 				<span className="text-2xl font-heading -ml-1 group-hover:text-blue-600 transition-colors duration-300">
 					Todografo
 				</span>
 			</a>
+
+			{/* Desktop Nav */}
 			<div className="hidden lg:flex space-x-10 text-gray-600 items-center justify-end">
 				{navLinks.map((link) => (
 					<NavLink key={link.name} href={link.href}>
@@ -37,19 +40,21 @@ export const Navbar = () => {
 			{/* Mobile Nav */}
 			<div className="flex lg:hidden items-center justify-end">
 				<Hamburger color="#2563eb" size={24} toggled={isOpen} toggle={setIsOpen} />
-				{isOpen && (
-					<div className="absolute top-full w-screen left-0 right-0 flex flex-col space-y-4 bg-opacity-70 bg-white backdrop-filter backdrop-blur-lg py-3 shadow-sm">
-						<div onClick={handleClick} className="flex flex-col space-y-2 w-mobile m-auto font-semibold">
-							{navLinks.map((link) => (
-								<NavLink key={link.name} href={link.href}>
-									{link.name}
-									<hr className="border-gray-300 mt-1" />
-								</NavLink>
-							))}
-							<NavLink href="/signIn">Iniciar sesión</NavLink>
-						</div>
+				<div
+					className={`absolute top-full w-screen left-0 right-0 flex flex-col bg-opacity-70 bg-white backdrop-filter backdrop-blur-lg shadow-sm transition-all duration-[350ms] overflow-hidden ${
+						isOpen ? 'max-h-96' : 'max-h-0'
+					}`}
+				>
+					<div className="flex flex-col space-y-4 py-3 w-mobile m-auto font-medium" onClick={handleClick}>
+						{navLinks.map((link) => (
+							<NavLink key={link.name} href={link.href}>
+								{link.name}
+								<hr className="border-gray-300 mt-1" />
+							</NavLink>
+						))}
+						<NavLink href="/signIn">Iniciar sesión</NavLink>
 					</div>
-				)}
+				</div>
 			</div>
 		</nav>
 	);
