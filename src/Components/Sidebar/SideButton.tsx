6eb129869e7351b1d6a0a3a97bdbe4baca53c760
Lautaro Rodriguez Brown <lautanba@gmail.com>
2025-01-16 14:59:50 -0300

    Cambios al sideButton para manejar el logout

--- src/Components/Sidebar/SideButton.tsx
+++ src/Components/Sidebar/SideButton.tsx
@@ -3,18 +3,17 @@ import { FC, ComponentProps } from 'react';
 interface Props {
 	ariaLabel: string;
 	icon: FC<ComponentProps<'svg'>>;
+	onClick?: () => void;
 }
 
-export const SideButton: FC<Props> = ({ ariaLabel, icon: Icon }) => {
+export const SideButton: FC<Props> = ({ ariaLabel, icon: Icon, onClick }) => {
 	return (
-		<a
+		<button
 			aria-label={ariaLabel}
-			href="https://github.com/LautaRB/Todo-list-Front"
-			target="_blank"
-			rel="noopener noreferrer"
+			onClick={onClick}
 			className="icon-btn"
 		>
 			<Icon className="h-auto w-6 text-zinc-600 dark:text-zinc-400" />
-		</a>
+		</button>
 	);
 };
